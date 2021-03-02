af88acc3790d3d685145789d23b5215432aaf9fc
Maurice Perry <maurice@perry.productions>
2021-03-02 23:37:14 +0100

    Initial commit

--- src/main/java/org/tastefuljava/messages/xml/TextBuilder.java
+++ src/main/java/org/tastefuljava/messages/xml/TextBuilder.java
@@ -6,16 +6,18 @@ public class TextBuilder {
     private final StringBuilder buf = new StringBuilder();
     private boolean wasBlank;
     private boolean skipBlank = true;
+    private int eolCount;
 
     public void start(boolean trimHead) {
         buf.setLength(0);
         wasBlank = false;
         skipBlank = trimHead;
+        eolCount = 0;
     }
 
     public String end(boolean trimTail) {
-        if (wasBlank && !skipBlank && !trimTail) {
-            buf.append(' ');
+        if (!trimTail) {
+            addSpace();
         }
         return buf.toString();
     }
@@ -27,16 +29,24 @@ public class TextBuilder {
             char c = ch[i];
             if (Character.isWhitespace(c)) {
                 wasBlank = true;
-            } else {
-                if (wasBlank) {
-                    if (!skipBlank) {
-                        buf.append(' ');
-                    }
-                    wasBlank = false;
+                if (c == '\n') {
+                    ++eolCount;
                 }
+            } else {
+                addSpace();
                 buf.append(c);
                 skipBlank = false;
             }
+        }
+    }
+
+    private void addSpace() {
+        if (wasBlank) {
+            if (!skipBlank) {
+                buf.append(eolCount > 1 ? '\n' : ' ');
+            }
+            wasBlank = false;
+            eolCount = 0;
         }
     }
 }
